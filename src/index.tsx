8f111d1ca03b9694b9a4f7d10315996397c507ce
sdegutis <sbdegutis@gmail.com>
2025-05-23 14:37:42 -0500

    Consolidate sample.

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,5 @@
-// import { foo as foo2 } from 'api.90s.dev/test1.js'
-// import React from 'react'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-
-// export const foo = foo2 + 1
 
 function Foo() {
   const [clicks, setClicks] = React.useState(0)
@@ -18,7 +14,3 @@ function Foo() {
 document.body.innerHTML = '<div id="app"></div>'
 const root = createRoot(document.getElementById('app')!)
 root.render(<Foo />)
-
-// export * as bla from 'api.90s.dev/test2.js'
-// export * as bla2 from './test2.js'
-
